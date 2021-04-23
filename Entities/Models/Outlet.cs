c8c27ea45a4dc06e1025c8ca95b634a11d08cbb6
S t <stefan_devop@outlook.com>
2021-04-23 14:11:27 +0200

    Added Outlet model

---
+++ Entities/Models/Outlet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TwinPalmsApi.Models
+{
+    public partial class Outlet
+    {
+        // TODO
+        //public Outlet()
+        //{
+        //    FbReports = new HashSet<FbReport>();
+        //    OutletUsers = new HashSet<OutletUser>();
+        //}
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // TODO
+        //public int CompanyId { get; set; }
+
+        //public virtual Company Company { get; set; }
+        //public virtual ICollection<FbReport> FbReports { get; set; }
+        //public virtual ICollection<OutletUser> OutletUsers { get; set; }
+    }
+}
