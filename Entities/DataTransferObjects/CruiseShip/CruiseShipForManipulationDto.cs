4af8051d1b1169024977786b126f00d71a58ad86
S t <stefan_devop@outlook.com>
2021-04-27 00:00:56 +0200

    Fixed typo

--- Entities/DataTransferObjects/CruiseShip/CruiseShipForManipulationDto.cs
+++ Entities/DataTransferObjects/CruiseShip/CruiseShipForManipulationDto.cs
@@ -8,7 +8,7 @@ namespace Entities.DataTransferObjects
 {
     public abstract class CruiseShipForManipulationDto
     {
-        [Required(ErrorMessage = "Cruise ship name is a required field.")]
+        [Required(ErrorMessage = "CruiseShip name is a required field.")]
         [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
         public string Name { get; set; }
         public int CruiseCompanyId { get; set; }
