1bce6771378922a9760fc7c4f8acb1b039ff729a
Henrik <web@villprogrammera.nu>
2021-05-05 11:34:59 +0200

    OutletUserConfig och en massa [JsonIgnore]

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Entities.Models
 {
@@ -12,12 +13,17 @@ namespace Entities.Models
         }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<FbReport> FbReports { get; set; }
+        [JsonIgnore]
         public virtual ICollection<HotelUser> HotelUsers { get; set; }
+        [JsonIgnore]
         public virtual ICollection<OtherReport> OtherReports { get; set; }
+        [JsonIgnore]
         public virtual ICollection<OutletUser> OutletUsers { get; set; }
+        [JsonIgnore]
         public virtual ICollection<RoomsReport> RoomsReports { get; set; }
+        [JsonIgnore]
         public virtual ICollection<UserRole> UserRoles { get; set; }
         
     }
