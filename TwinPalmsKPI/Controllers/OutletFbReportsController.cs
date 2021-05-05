8aa8d9efbaead9056f15c7eed964dc62d0ace108
S t <stefan_devop@outlook.com>
2021-05-05 19:23:28 +0200

    ---

--- TwinPalmsKPI/Controllers/OutletFbReportsController.cs
+++ TwinPalmsKPI/Controllers/OutletFbReportsController.cs
@@ -29,11 +29,12 @@ namespace TwinPalmsKPI.Controllers
             _mapper = mapper;
         }
 
+        // ************************************* GetOutletFbReport ***********************************************
         /// <summary>
         /// Gets a All FbReports by Outlet Id between two Dates
         /// </summary>
         /// <remarks>
-        /// Gets all FbReports between fromDate and toDate, including toDate
+        /// Gets all FbReports for one outlet between fromDate and toDate, including toDate
         /// 
         /// For example: to get the reports from only 2021-05-04
         /// 
@@ -41,7 +42,7 @@ namespace TwinPalmsKPI.Controllers
         ///     toDate = 2021-05-04
         ///     
         /// </remarks>     
-        [HttpGet("/outlet/{outletId}/fbReports", Name = "OutletFbReportsByIdAndDate")]
+        [HttpGet("/outlet/fbReports/{outletId}", Name = "OutletFbReportsByIdAndDate")]
         public async Task<IActionResult> GetOutletFbReport(int outletId, DateTime fromDate, DateTime toDate)
         {
             // Needed to include also reports from toDate day
@@ -82,6 +83,62 @@ namespace TwinPalmsKPI.Controllers
             }).ToArray();
 
             return Ok(outletFbReportsToReturn);
+        }
+
+        /// <summary>
+        /// Gets a All FbReports by Outlet Ids between two Dates
+        /// </summary>
+        /// <remarks>
+        /// Gets all FbReports for many outlets between fromDate and toDate, including toDate
+        /// 
+        /// For example: to get the reports from only 2021-05-04
+        /// 
+        ///     fromDate = 2021-05-04
+        ///     toDate = 2021-05-04
+        ///     
+        /// </remarks>     
+        [HttpGet("/outlets/fbReports", Name = "OutletsFbReportsByIdAndDate")]
+        public async Task<IActionResult> GetOutletFbReports( [FromQuery] int[] outletIds, DateTime fromDate, DateTime toDate)
+        {
+            //// Needed to include also reports from toDate day
+            //DateTime toDateCorrected = toDate.AddDays(1);
+
+            //var outlet = await _repository.Outlet.GetOutletAsync(outletId, trackChanges: false);
+
+            //if (outlet == null)
+            //{
+            //    _logger.LogInfo($"Outlets with id {outletId} doesn't exist in the database.");
+            //    return NotFound();
+            //}
+
+            //var outletFbReports = await _repository.FbReport.GetAllOutletFbReportsForOneOutlet(outletId, fromDate, toDateCorrected, trackChanges: false);
+
+            //if (outletFbReports == null)
+            //{
+            //    _logger.LogInfo($"No reports for Outlet with id {outletId} found in the database between {fromDate} and {toDateCorrected}.");
+            //    return NotFound();
+            //}
+
+            //var outletFbReportsToReturn = outletFbReports.Select(o => new
+            //{
+            //    Tables = o.Tables,
+            //    Food = o.Food,
+            //    Beverage = o.Beverage,
+            //    OtherIncome = o.OtherIncome,
+            //    Date = o.Date,
+            //    GuestsFromHotel = o.GuestsFromHotel,
+            //    GuestsFromOutsideHotel = o.GuestsFromOutsideHotel,
+            //    IsPublicHoliday = o.IsPublicHoliday,
+            //    Notes = o.Notes,
+            //    OutletId = o.OutletId,
+            //    UserId = o.UserId,
+            //    LocalEventId = o.LocalEventId,
+            //    GuestSourceOfBusinesses = o.FbReportGuestSourceOfBusinesses.Select(f => f.GuestSourceOfBusiness).ToList(),
+            //    Weathers = o.WeatherFbReports.Select(w => w.Weather).ToList()
+            //}).ToArray();
+
+            //return Ok(outletFbReportsToReturn);
+            return Ok(); ////////////////////////
         }
     }
 }
