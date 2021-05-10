a72d4b50f50ece51394441131d781ba762509619
Henrik <web@villprogrammera.nu>
2021-05-10 17:35:23 +0200

    use html in emails

--- EmailService/EmailSender.cs
+++ EmailService/EmailSender.cs
@@ -30,7 +30,7 @@ namespace EmailService
             emailMessage.From.Add(new MailboxAddress(_emailConfig.From));
             emailMessage.To.AddRange(message.To);
             emailMessage.Subject = message.Subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = string.Format("<hp style='color:blue;'>{0}</p>", message.Content) };
 
             return emailMessage;
         }
