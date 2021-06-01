a5cddc3cdf94a405e59c7a0da84241da3c3577ab
Anette Kniberg <anettekniberg@gmail.com>
2021-06-01 15:08:32 +0200

    Fixed bug with reports getting saved, despite error response.

--- TwinPalmsKPI/Controllers/FpReportsController.cs
+++ TwinPalmsKPI/Controllers/FpReportsController.cs
@@ -181,7 +181,7 @@ namespace TwinPalmsKPI.Controllers
                         fbReportEntity.ImagePath = dbPath;
                         _logger.LogInfo("entity to be created: " + fbReportEntity.ImagePath);
                         _repository.FbReport.CreateFbReport(fbReportEntity);
-                        await _repository.SaveAsync();
+                        //await _repository.SaveAsync(); TODO: May we delete? This caused fbreports to be saved before validation- wrongful reports gets saved.
                     }
                 }
                 else
